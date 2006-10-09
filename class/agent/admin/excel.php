ff318f2634dbb6c2568052a7a39d097923a737d8
grekas20 <grekas20@1d68caee-3c19-0410-8b00-a601b06dc6dc>
2006-10-09 10:22:16 +0000

    * fixed charset issue
    git-svn-id: https://svn.intellagence.eu/enquete@12 1d68caee-3c19-0410-8b00-a601b06dc6dc

--- class/agent/admin/excel.php
+++ class/agent/admin/excel.php
@@ -16,17 +16,15 @@ class extends agent_admin
 		$enquete = $this->enquete->enquete;
 
 		$workbook = new Spreadsheet_Excel_Writer();
-		$workbook->setVersion(8);
 		$workbook->send("enquete_{$enquete}.xls");
 		$worksheet = $workbook->addWorksheet('Resultats');
-		$worksheet->setInputEncoding('UTF-8');
 
 		$format = $workbook->addFormat();
 		$format->setBold();
 
 		$sql = "SHOW COLUMNS FROM enquete_{$enquete}";
 		$entete = new loop_sql($sql);
-		for ($i = 0; $a = $entete->loop(); ++$i) $worksheet->writeString(0, $i, $a->Field, $format);
+		for ($i = 0; $a = $entete->loop(); ++$i) $worksheet->write(0, $i, $a->Field, $format);
 
 		$sql = "SELECT * FROM enquete_{$enquete}
 			WHERE result_id IN (SELECT result_id FROM admin_user WHERE statut='enregistre')";
@@ -36,7 +34,7 @@ class extends agent_admin
 			$j = 0;
 			foreach ($a as &$value)
 			{
-				$worksheet->writeString($i, $j, $value);
+				$worksheet->write($i, $j, $this->format($value));
 				++$j;
 			}
 		}
@@ -44,5 +42,12 @@ class extends agent_admin
 		$workbook->close();
 
 		return $o;
+	}
+
+	function format($value)
+	{
+		$value = mb_convert_encoding($value, 'ISO-8859-1');
+		$value = str_replace(array("\r\n", "\r"), array("\n", "\n"), $value);
+		return $value;
 	}
 }
