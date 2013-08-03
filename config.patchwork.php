b80e20e7355f011d59040666dc15127673a03d7a
espci.org <postmaster@espci.org>
2013-08-03 18:24:46 +0200

    explicit Patchwork modules

--- config.patchwork.php
+++ config.patchwork.php
@@ -1,4 +1,9 @@
 <?php
 
+#patchwork __patchwork__/pieces/pMail
+#patchwork __patchwork__/pieces/doctrine
+#patchwork __patchwork__/pieces/pForm/pieces/*
+#patchwork __patchwork__/pieces/agent-controller
+
 $CONFIG += array('clientside' => false);
 $CONFIG += PCORG::DSN('enquete', 'doctrine.');
